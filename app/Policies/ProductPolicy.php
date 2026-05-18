0bce159f26e71790a37160474b8dfedf3bdf468b
Muad-Ahmed <muad8010@gmail.com>
2026-05-18 10:18:57 +0300

    feat: add product policy

--- app/Policies/ProductPolicy.php
+++ app/Policies/ProductPolicy.php
@@ -11,12 +11,12 @@ class ProductPolicy
     {
         // TODO: تحقق أن المستخدم بائع وأن product->vendor_profile_id يطابق vendor profile الخاص به.
         // TODO: أعد true فقط عندما يكون المالك هو نفسه؛ هذه أهم حماية في تطبيق متعدد البائعين.
-        return false;
+        return $user->vendorProfile &&  $product->vendor_profile_id === $user->vendorProfile->id;
     }
 
     public function delete(User $user, Product $product): bool
     {
         // TODO: استخدم نفس قاعدة الملكية، ويمكنك لاحقا منع الحذف إذا كان المنتج داخل طلب مكتمل.
-        return false;
+        return $user->vendorProfile &&  $product->vendor_profile_id === $user->vendorProfile->id;
     }
 }
