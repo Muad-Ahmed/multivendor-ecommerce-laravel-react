98e3f149b226761915a1d628d43db033e8c582cb
Muad-Ahmed <muad8010@gmail.com>
2026-05-19 09:54:52 +0300

    feat: implement the relationships between factories in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\ProductImage;
 use App\Models\User;
+use App\Models\VendorProfile;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +19,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $categories = Category::factory(8)->create();
+
+
+        User::factory(10)->customer()->create();
+
+        User::factory()->admin()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
         ]);
+
+        User::factory(5)
+            ->vendor()
+            ->has(
+                VendorProfile::factory()
+                    ->has(
+                        Product::factory(10)
+                            ->state(fn() => ['category_id' => $categories->random()->id])
+                            ->has(ProductImage::factory(3), 'productImages')
+                    )
+            )
+            ->create();
+
     }
 }
